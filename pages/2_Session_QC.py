17cbe52abfcbbf701eb8c86d4a4fa1851e705a38
lawtj <41483192+lawtj@users.noreply.github.com>
2024-07-12 17:54:34 -0700

    revision of full QC workflow

--- pages/2_Session_QC.py
+++ pages/2_Session_QC.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 import ast
 from redcap import Project
 import io
+import os
+import openox as ox
+from session_functions import colormap
+
 
 def getdf():
     api_url = 'https://redcap.ucsf.edu/api/'
@@ -16,6 +21,112 @@ def getdf():
 
 df = getdf()
 
+def get_qc_status():
+    api_url = 'https://redcap.ucsf.edu/api/'
+    api_k = st.secrets['REDCAP_QC']
+    proj = Project(api_url, api_k)
+    df = proj.export_records(format_type='df')
+
+    return df
+
+qc_status = get_qc_status()
+
+def get_labview_samples():
+    api_url = 'https://redcap.ucsf.edu/api/'
+    try:
+        api_k = st.secrets['api_k']
+    except:
+        api_k = os.environ['REDCAP_FILE_RESPOSITORY']
+    proj = Project(api_url, api_k)
+    f = io.BytesIO(proj.export_file(record='8', field='file')[0])
+    labview_samples = pd.read_csv(f)
+    return labview_samples
+
+labview_samples = get_labview_samples()
+
+def create_subset_frame(labview_samples, selected_session, show_cleaned=False):
+    frame = labview_samples[labview_samples['session'] == selected_session]
+    frame = frame.drop(columns=['session'])
+    
+    if frame['Nellcor PM1000N-1/SpO2'].sum() > 0:
+        frame = frame.drop(columns=['Masimo 97/SpO2', 'Nellcor/SpO2', 'Nellcor/SpO2_diff_prev', 'Nellcor/SpO2_diff_next', 'Masimo 97/SpO2_diff_prev', 'Masimo 97/SpO2_diff_next'])
+        frame = frame.rename(columns={
+            'Nellcor PM1000N-1/SpO2': 'Nellcor/SpO2',
+            'Nellcor PM1000N-1/SpO2_diff_prev': 'Nellcor/SpO2_diff_prev',
+            'Nellcor PM1000N-1/SpO2_diff_next': 'Nellcor/SpO2_diff_next',
+            'Rad97-60/SpO2': 'Masimo 97/SpO2',
+            'Rad97-60/SpO2_diff_prev': 'Masimo 97/SpO2_diff_prev',
+            'Rad97-60/SpO2_diff_next': 'Masimo 97/SpO2_diff_next'
+        })
+    else:
+        frame = frame.drop(columns=['Masimo HB/SpO2', 'Masimo HB/SpO2_diff_prev', 'Masimo HB/SpO2_diff_next', 'Nellcor PM1000N-1/SpO2', 'Nellcor PM1000N-1/SpO2_diff_prev', 'Nellcor PM1000N-1/SpO2_diff_next', 'Rad97-60/SpO2', 'Rad97-60/SpO2_diff_prev', 'Rad97-60/SpO2_diff_next'])
+    
+    if not show_cleaned:
+        # Add logic to set so2 and Nellcor/SpO2 to np.nan based on their stability
+        frame['so2'] = frame.apply(lambda row: row['so2'] if row['so2_stable'] else np.nan, axis=1)
+        frame['Nellcor/SpO2'] = frame.apply(lambda row: row['Nellcor/SpO2'] if row['Nellcor_stable'] else np.nan, axis=1)
+    
+    criteria_check_tuple, criteria_check_df = ox.session_criteria_check(frame)
+    
+    # Create column 'col_so2_symbol' which is colormap['column'][1] if so2_stable is True, else 'cross'
+    frame['so2_symbol'] = frame['so2_stable'].apply(lambda x: colormap['so2'][1] if x else 'cross')
+    frame['Nellcor/SpO2_symbol'] = frame['Nellcor_stable'].apply(lambda x: colormap['Nellcor/SpO2'][1] if x else 'cross')
+    
+    # bias should be circle unless bias is greater than max_bias then it should be other
+    frame['bias_symbol'] = 'circle'
+    
+    # ACTUALLY so2_line is always DarkSlateGrey
+    frame['so2_line'] = 'DarkSlateGrey'
+    
+    # same for Nellcor/SpO2
+    frame['Nellcor/SpO2_line'] = np.where((frame['Nellcor_stable'] == True) & (abs(frame['bias']) > max_bias), 'red', 'DarkSlateGrey')
+    
+    # bias line should be blue if bias is greater than max_bias
+    frame['bias_line'] = np.where(abs(frame['bias']) > max_bias, 'blue', 'DarkSlateGrey')
+    
+    return frame, criteria_check_tuple, criteria_check_df
+
+plotcolumns = ['so2', 'Nellcor/SpO2','bias']
+
+def create_plot(frame, plotcolumns):
+    fig = go.Figure()
+    for column in plotcolumns:
+        fig.add_trace(go.Scatter(
+            x=frame['sample'], y=frame[column],
+            mode='markers',
+            name=column,
+            marker=dict(
+                symbol= frame[column + '_symbol'],
+                color= colormap[column][0],
+                size=12,
+                opacity=0.8,
+                line=dict(width=1.5, color=frame[column + '_line'])
+            ),
+        ))
+    return fig
+
+
+def update_qc_field(df, session_id, ):
+    # first, check if qc_complete is True, but if any of the other fields are False, then qc_complete should be False
+    # print each
+    print(st.session_state.qc_notes, st.session_state.qc_missing, st.session_state.qc_date_discrepancy, st.session_state.qc_id_discrepancy, st.session_state.qc_quality, st.session_state.qc_complete)
+    print(all([st.session_state.qc_notes, st.session_state.qc_missing, st.session_state.qc_date_discrepancy, st.session_state.qc_id_discrepancy, st.session_state.qc_quality]))
+    if st.session_state.qc_complete and not all([st.session_state.qc_notes, st.session_state.qc_missing, st.session_state.qc_date_discrepancy, st.session_state.qc_id_discrepancy, st.session_state.qc_quality]):
+        qc_message.error('Cannot set QC complete, some issues are not resolved.')
+        return
+    api_url = 'https://redcap.ucsf.edu/api/'
+    api_k = st.secrets['REDCAP_QC']
+    proj = Project(api_url, api_k)
+    update_df = df[df['session_id'] == session_id]
+    update_df['session_notes_addressed'] = st.session_state.qc_notes
+    update_df['missing_files_resolved'] = st.session_state.qc_missing
+    update_df['date_discrepancies_resolved'] = st.session_state.qc_date_discrepancy
+    update_df['id_discrepancies_resolved'] = st.session_state.qc_id_discrepancy
+    update_df['data_quality_checked'] = st.session_state.qc_quality
+    update_df['qc_complete'] = st.session_state.qc_complete
+    proj.import_records(update_df, import_format='df')
+    qc_message.success('QC status updated', icon='✅')
+    qc_message.caption(pd.Timestamp.now())
 ####################### title #######################
 
 st.title('Session Quality Control')
@@ -26,59 +137,78 @@ df['missing_ptids_tf'] = df['missing_ptids'].apply(lambda x: len(x)>0)
 df['missing_abg_tf'] = df['missing_ptids'].apply(lambda x: 'Blood Gas' in x)
 df['missing_konica_tf'] = df['missing_ptids'].apply(lambda x: 'Konica' in x)
 
-# st.dataframe(df, hide_index=True)
-
 ####################### filters #######################
 
-show_date_issues = st.checkbox('Show sessions with date discrepancies', value=False)
-show_missing_dates = st.checkbox('Show sessions with missing dates', value=False)
-show_ptid_issues = st.checkbox('Show sessions with patient ID discrepancies', value=True)
-show_missing_ptids = st.checkbox('Show sessions with missing patient IDs', value=False)
-show_missing_abgs = st.checkbox('Show sessions with missing ABG data', value=False)
-show_missing_konica = st.checkbox('Show sessions with missing Konica data', value=False)
+qc_complete_sessions_list = qc_status[qc_status['qc_complete'] == 1]['session_id'].tolist()
+qc_incomplete_sessions_list = qc_status[qc_status['qc_complete'] == 0]['session_id'].tolist()
+
+show_qc_status = st.selectbox('Select QC Status', ['Incomplete', 'Complete'])
+
+left, right = st.columns(2)
+
+with left:
+    show_date_issues = st.checkbox('Show sessions with date discrepancies', value=False)
+    show_missing_dates = st.checkbox('Show sessions with missing dates', value=False)
+    show_ptid_issues = st.checkbox('Show sessions with patient ID discrepancies', value=True)
+
+with right:
+    show_missing_ptids = st.checkbox('Show sessions with missing patient IDs', value=False)
+    show_missing_abgs = st.checkbox('Show sessions with missing ABG data', value=False)
+    show_missing_konica = st.checkbox('Show sessions with missing Konica data', value=False)
 dfstats = df.copy()
 
 # Filtering logic
-if not show_date_issues and not show_ptid_issues and not show_missing_dates and not show_missing_ptids and not show_missing_abgs and not show_missing_konica:
-    df = df  # Show all data if no filter is selected
+if not show_date_issues and not show_ptid_issues and not show_missing_dates and not show_missing_ptids and not show_missing_abgs and not show_missing_konica and show_qc_status == 'All':
+    df = df  # Show all data if no filter is selected and QC status is 'All'
 else:
     # Create masks based on user selection
-    mask = pd.Series([False] * len(df))  # Initialize mask to False
+    mask = pd.Series([True] * len(df))  # Initialize mask to True
     if show_date_issues:
-        mask |= df['date_issues_tf']  # Update mask for date issues
+        mask &= df['date_issues_tf']  # Update mask for date issues
     if show_ptid_issues:
-        mask |= df['patient_id_issues_tf']  # Update mask for patient ID issues
+        mask &= df['patient_id_issues_tf']  # Update mask for patient ID issues
     if show_missing_dates:
-        mask |= df['missing_dates_tf']
+        mask &= df['missing_dates_tf']
     if show_missing_ptids:
-        mask |= df['missing_ptids_tf']
+        mask &= df['missing_ptids_tf']
     if show_missing_abgs:
-        mask |= df['missing_abg_tf']
+        mask &= df['missing_abg_tf']
     if show_missing_konica:
-        mask |= df['missing_konica_tf']
+        mask &= df['missing_konica_tf']
     
+    # Apply QC status filter
+    if show_qc_status == 'Complete':
+        mask &= df['session_id'].isin(qc_complete_sessions_list)
+    elif show_qc_status == 'Incomplete':
+        mask &= df['session_id'].isin(qc_incomplete_sessions_list)
 
     df = df[mask]  # Apply the mask
 
-selected_session = st.selectbox('Select Session ID', df['session_id'].sort_values(ascending=False))
+selected_session = st.selectbox('Select Session ID', df['session_id'].sort_values(ascending=False), key='selected_session')
 
 
 datesdict = df.loc[df['session_id'] == selected_session,'dates'].values[0]
 ptids = df.loc[df['session_id'] == selected_session, 'patient_ids'].values[0]
+###################### Initialize Session State #######################
+st.session_state.qc_notes = qc_status.loc[qc_status['session_id'] == selected_session, 'session_notes_addressed'].values[0]
+st.session_state.qc_missing = qc_status.loc[qc_status['session_id'] == selected_session, 'missing_files_resolved'].values[0]
+st.session_state.qc_date_discrepancy = qc_status.loc[qc_status['session_id'] == selected_session, 'date_discrepancies_resolved'].values[0]
+st.session_state.qc_id_discrepancy = qc_status.loc[qc_status['session_id'] == selected_session, 'id_discrepancies_resolved'].values[0]
+st.session_state.qc_quality = qc_status.loc[qc_status['session_id'] == selected_session, 'data_quality_checked'].values[0]
+st.session_state.qc_complete = qc_status.loc[qc_status['session_id'] == selected_session, 'qc_complete'].values[0]
 
 ####################### layout #######################
 
+st.dataframe(df[['session_id','session_notes','date_issues_tf','patient_id_issues_tf','missing_abg_tf','missing_konica_tf','missing_ptids']].sort_index(ascending=False), use_container_width=True, column_config={
+    "session_notes": st.column_config.TextColumn("Session Notes", width='medium'),
+    "date_issues_tf": st.column_config.CheckboxColumn("Date Issues", width='small'),
+    "patient_id_issues_tf": st.column_config.CheckboxColumn("Patient ID Issues", width='small'),
+    'missing_ptids': st.column_config.ListColumn('Missing Patient IDs', width='medium'),
+    'missing_abg_tf': st.column_config.CheckboxColumn("Missing ABG"),
+    'missing_konica_tf':st.column_config.CheckboxColumn('Missing Konica')},hide_index=True)
 
-tab_overview, tab_details = st.tabs(['Overview', 'Details'])
-
-with tab_overview:
-    st.dataframe(df[['session_id','session_notes','date_issues_tf','patient_id_issues_tf','missing_abg_tf','missing_konica_tf','missing_ptids']].sort_index(ascending=False), use_container_width=True, column_config={
-        "session_notes": st.column_config.TextColumn("Session Notes", width='medium'),
-        "date_issues_tf": st.column_config.CheckboxColumn("Date Issues", width='small'),
-        "patient_id_issues_tf": st.column_config.CheckboxColumn("Patient ID Issues", width='small'),
-        'missing_ptids': st.column_config.ListColumn('Missing Patient IDs', width='medium'),
-        'missing_abg_tf': st.column_config.CheckboxColumn("Missing ABG"),
-        'missing_konica_tf':st.column_config.CheckboxColumn('Missing Konica')},hide_index=True)
+with st.sidebar:
+    max_bias = st.number_input('Highlight points where maximum bias is >= :', 0, 20, 10, 1)
 
     st.write('Total number of sessions:', len(dfstats))
     st.write('Number of sessions with date discrepancies:', len(dfstats[dfstats['date_issues_tf']]))
@@ -86,25 +216,48 @@ with tab_overview:
     st.write('Number of sessions with missing ABG data:', len(dfstats[dfstats['missing_abg_tf']]))
     st.write('Number of sessions with missing Konica data:', len(dfstats[dfstats['missing_konica_tf']]))
 
-with tab_details:
 
-    st.markdown(f'## Session {selected_session}')
+st.markdown(f'## Session {selected_session}')
+show_cleaned = st.checkbox('Show cleaned out points', value=False, key='show_cleaned')
 
-    st.markdown('### Notes')
-    if pd.isna(df.loc[df['session_id']==selected_session,'session_notes'].values[0]):
-        st.write('No notes found')
-    else:
-        st.write(df.loc[df['session_id']==selected_session,'session_notes'].values[0])
-
+with st.form(key='qc_form'):
     left, right = st.columns(2)
-
     with left:
-        st.markdown('### Dates')
+
+        st.markdown('### Session Notes')
+        st.checkbox('Session notes addressed', key='qc_notes')
+
+        if pd.isna(df.loc[df['session_id']==selected_session,'session_notes'].values[0]):
+            st.success('No notes found', icon="✅")
+        else:
+            st.info(df.loc[df['session_id']==selected_session,'session_notes'].values[0])
+        
+
+    with right:
+        st.markdown('### Missing Files')
+        st.checkbox('Missing files addressed', key='qc_missing')
+
+        missing_abg = df.loc[df['session_id']==selected_session,'missing_abg_tf'].values[0]
+        missing_konica = df.loc[df['session_id']==selected_session,'missing_konica_tf'].values[0]
+
+        if missing_abg:
+            st.error('Missing ABG file')
+        if missing_konica:
+            st.error('Missing Konica file')
+        if not missing_abg and not missing_konica:
+            st.success('No missing files found', icon="✅")
+
+        
+    left, right = st.columns(2)
+    with left:
+        st.markdown('### Date Discrepancies')
+        st.checkbox('Date Discrepancies addressed', key='qc_date_discrepancy')
+
         # make a warning flag if there is date_issues_tf = True
         if df.loc[df['session_id']==selected_session, 'date_issues_tf'].values[0]:
-            st.error('Date issues found', icon="🚨")
+            st.error('Date discrepancies found', icon="🚨")
         else:
-            st.success('No date issues found', icon="✅")
+            st.success('No date discrepancies found', icon="✅")
         st.markdown(f'''
         
         * **Session date:** {datesdict['dates']['session']} 
@@ -115,14 +268,15 @@ with tab_details:
 
         * **Manual pulse ox entry date:** {datesdict['dates']['pulseox'] if datesdict['dates']['pulseox'] != [] else 'No manual pulse ox entry data'}
         ''')
-
     with right:
-        st.markdown('### Patient ID Issues')
+        st.markdown('### Patient ID Discrepancies')
+        st.checkbox('Patient ID Discrepancies addressed', key='qc_id_discrepancy')
+
         # make a warning flag if there is patient_id_issues_tf = True
         if df.loc[df['session_id']==selected_session,'patient_id_issues_tf'].values[0]:
-            st.error('Patient ID issues found', icon="🚨")
+            st.error('Patient ID discrepancies found', icon="🚨")
         else:
-            st.success('No patient ID issues found', icon="✅")
+            st.success('No patient ID discrepancies found', icon="✅")
 
         st.markdown(f'''
         * **Session patient ID**: {ptids['patient_ids']['session']} 
@@ -133,4 +287,34 @@ with tab_details:
 
         * **Manual pulse ox entry patient ID**: {ptids['patient_ids']['pulseox']}
         ''')
+    
+    st.markdown('### Data Quality Check')
+    st.checkbox('Data quality checked (no bad points)', key='qc_quality')
+    st.write('ARMS calculations:')
+    for value in df.loc[df['session_id'] == selected_session, 'arms'].values[0].items():
+        st.write(value[0], round(value[1],2))
+
+    st.plotly_chart(create_plot(create_subset_frame(labview_samples, selected_session, st.session_state.show_cleaned)[0], plotcolumns), use_container_width=True)
+    st.dataframe(create_subset_frame(labview_samples, selected_session)[0].set_index('sample')
+                 .drop(columns=['sample_diff_prev',
+                                'sample_diff_next',
+                                'Nellcor/SpO2_diff_prev',
+                                'Nellcor/SpO2_diff_next',
+                                'Masimo 97/SpO2_diff_prev',
+                                'Masimo 97/SpO2_diff_next',
+                                'so2_symbol',
+                                'Nellcor/SpO2_symbol',
+                                'bias_symbol',
+                                'so2_line',
+                                'Nellcor/SpO2_line',
+                                'bias_line']), use_container_width=True)
+    
+    st.markdown('### Final QC')
+    st.checkbox('QC complete', key='qc_complete')
+    qc_message = st.container()
+    
+    submit_button = st.form_submit_button('Save QC', on_click=update_qc_field, args=(qc_status, selected_session))
+
+
+
 
---
+++ session_functions.py
@@ -0,0 +1,163 @@
+import plotly.graph_objects as go
+import numpy as np
+
+def arms(spo2,sao2):
+    return np.sqrt(np.mean((spo2-sao2)**2))
+
+
+def threesamples(abg):
+    """
+    Applies to ABG dataframe
+
+    goal: if the CRC was doing 3 runs for the blood sample, throw out the so2 value that is off
+    group by encountercol and sample and count the number of so2 values
+    if there are only two so2 values, then the so2 range is the difference between the two so2 values
+    else if there are more than two samples, calculate the so2 range for the two samples that has so2 value less than 0.5 apart.
+    else if there are no samples that are less than 0.5 apart, then the so2 range is the difference between the highest and lowest so2 values - werid if has
+
+    """
+    exclude_list = []
+
+    for group, sample in abg.groupby(['session','sample']):
+        #group is a tuple of (current session, current sample), and sample is the dataframe filtered 
+            ## --------------- calculate the difference between the median and the so2 value here
+            median_so2 = sample['so2'].median()
+            sample['diff'] = sample['so2'] - median_so2
+            ## --------------- if any difference greater than 1.5, we need to skip this sample - might be a trailing 0s problem or something else weird
+            # if len(sample[sample['diff'].abs() >= 1.5]) > 0:
+            #     # print(group[0])
+            #     abg.loc[(abg['session'] == group[0]) & (abg['sample'] == group[1]), 'so2_range'] = sample['so2'].max() - sample['so2'].min()
+            #     continue
+    
+            # now we've skipped those samples that have so2 values that are more than 1.5 apart
+            if len(sample) == 2:
+                # if the two so2 values are more than 1 apart, then we need to exclude this sample
+                if sample['so2'].max() - sample['so2'].min() > 1:
+                    exclude_list.append([group[0], group[1], sample['so2'].max(), sample['so2'].min(), sample['so2'].max() - sample['so2'].min()])
+                abg.loc[(abg['session'] == group[0]) & (abg['sample'] == group[1]), 'so2_range'] = sample['so2'].max() - sample['so2'].min()
+
+            elif len(sample) > 2:
+                try:
+                    # if none of the samples are less than 0.5 apart, then we need to exclude this sample
+                    if len(sample[sample['diff'].abs() <= 0.5]) < 2:
+                        abg.drop(abg[(abg['session'] == group[0]) & (abg['sample'] == group[1])].index, inplace=True)
+                    else:
+                        # if we do have two so2 values that are no more than 0.5 apart, drop the row where the difference is greater than 0.5
+                        abg.drop(sample[sample['diff'].abs() > 0.5].index, inplace=True)
+                        # now calculate the difference between the highest and lowest so2 values
+                        abg.loc[(abg['session'] == group[0]) & (abg['sample'] == group[1]), 'so2_range'] = sample['so2'].max() - sample['so2'].min()
+                except Exception as e:
+                    print('Exception!: ', e)
+
+    # ###---------------------------------------------------------------------------------------------------------------------------------------------------------###
+    # drop rows of where session and sample is in the exclude list
+    for row in exclude_list:
+        abg.drop(abg[(abg['session'] == row[0]) & (abg['sample'] == row[1])].index, inplace=True)
+
+def recalculate_so2_range(df, encountercol):
+    if 'so2_range' in df.columns:
+        df.drop('so2_range', axis=1, inplace=True)
+    ###---------------------------------------------------------------------------------------------------------------------------------------------------------###
+    for encounter in df['session'].unique():
+        df_encounter = df[df['session'] == encounter]
+        for sample in df_encounter['sample'].unique():
+            df_encounter_sample = df_encounter[df_encounter['sample'] == sample].copy()
+            
+            ## --------------- calculate the difference between the median and the so2 value here
+            median_so2 = df_encounter_sample['so2'].median()
+            df_encounter_sample['diff'] = df_encounter_sample['so2'] - median_so2
+            ## --------------- if any difference greater than 1.5, we need to skip this sample - might be a trailing 0s problem or something else weird
+            ## we comment out this part of the code now since we start reviewing session in the QA script 
+            # if len(df_encounter_sample[df_encounter_sample['diff'].abs() >= 1.5]) > 0:
+            #     df.loc[(df['session'] == encounter) & (df['sample'] == sample), 'so2_range'] = df_encounter_sample['so2'].max() - df_encounter_sample['so2'].min()
+            #     continue
+            
+            # now we skip those samples that have so2 values that are more than 1.5 apart
+            if len(df_encounter_sample) == 2:
+                df.loc[(df['session'] == encounter) & (df['sample'] == sample), 'so2_range'] = df_encounter_sample['so2'].max() - df_encounter_sample['so2'].min()
+            elif len(df_encounter_sample) > 2:
+                # if we do have two so2 values that are no more than 0.5 apart, drop the row where the difference is greater than 0.5
+                if len(df_encounter_sample[df_encounter_sample['diff'].abs() <= 0.5]) > 0:
+                    df.drop(df_encounter_sample[df_encounter_sample['diff'].abs() > 0.5].index, inplace=True)
+                    # now calculate the difference between the highest and lowest so2 values
+                    df.loc[(df['session'] == encounter) & (df['sample'] == sample), 'so2_range'] = df_encounter_sample['so2'].max() - df_encounter_sample['so2'].min()
+                else:
+                    df.loc[(df['session'] == encounter) & (df['sample'] == sample), 'so2_range'] = df_encounter_sample['so2'].max() - df_encounter_sample['so2'].min()
+    ###---------------------------------------------------------------------------------------------------------------------------------------------------------###
+    
+    so2_range = df.groupby([encountercol,'sample'])['so2'].agg(so2_range=lambda x: x.max() - x.min()).reset_index()
+    return df
+
+
+# Define a function to assign marker style based on the values of name_keep
+def assign_marker_style(name_keep):
+    if name_keep == True:
+        return 'circle'
+    elif name_keep == False:
+        return 'bowtie'
+    elif name_keep == False:
+        return 'hourglass'
+    elif name_keep == False:
+        return 'cross'
+
+
+colormap = {'Masimo 97/SpO2': ('IndianRed', 'circle'),
+            'masimo_bias': ('IndianRed', 'circle'),
+            'Nellcor/SpO2': ('palegreen', 'circle'),
+            'Nellcor_stable': ('palegreen', 'circle'),
+            'so2': ('powderblue', 'circle'),
+            'so2_range': ('powderblue', 'circle'),
+            'so2_stable': ('powderblue', 'circle'),
+            'bias': ('IndianRed', 'cross'),
+            'sao2': ('powderblue', 'circle'),
+            'spo2': ('palegreen', 'circle'),}
+
+def assign_marker_color(name_keep, column):
+    if name_keep == True:
+        return colormap[column]
+    else:
+        return 'purple'  # You can define additional conditions and colors as needed
+
+def create_scatter(frame, plotcolumns, stylecolumns):
+    fig = go.Figure()
+
+    # Adding traces with customized marker style based on name_keep
+    for plotcolumns, stylecolumns in zip(plotcolumns, stylecolumns):
+    # for column, keepcolumn in zip(['so2'], ['so2_compare']):
+        fig.add_trace(go.Scatter(
+            x=frame['sample'], y=frame[plotcolumns],
+            mode='markers',
+            name=plotcolumns,
+            marker=dict(
+                symbol=[assign_marker_style(style) for style in frame[stylecolumns]],
+                color=[assign_marker_color(style, stylecolumns) for style in frame[stylecolumns]],
+                size=12,
+                opacity=0.8,
+                line=dict(width=2, color='DarkSlateGrey')
+            ),
+            text=[plotcolumns, stylecolumns],
+        ))
+
+    # Update layout and show the plot
+    fig.update_layout(
+        template='plotly_white',
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
+    )
+
+    return fig
+
+def so2_compare(row):
+    if (row['manual_clean_so2'] == 'reject') & (row['so2_keep'] == 'keep'):
+        return 'manual rejection'
+    elif (row['manual_clean_so2'] == 'keep') & (row['so2_keep'] == 'reject'):
+        return 'algorithm rejection'
+    elif (row['manual_clean_so2'] == 'reject') & (row['so2_keep'] == 'reject'):
+        return 'reject'
+    else:
+        return 'keep'
